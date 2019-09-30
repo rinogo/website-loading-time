6f711aa6f95c38531037e89818c0bc2ec9e0b422
Rich Christiansen <rich@byu.net>
2019-09-30 15:00:40 -0600

    Improve error handling and output

--- website-loading-time.js
+++ website-loading-time.js
@@ -1,15 +1,19 @@
 //NightmareJS is powerful; check the documentation - https://github.com/segmentio/nightmare
 
 //Settings
-const TIMEOUT = 4000;
-const FREQUENCY = 5000;
+const FREQUENCY = 15000;
+const TIMEOUT = FREQUENCY - 1000;
+const LOADING_TIME_SANITY_CHECK_THRESHOLD = 100; //Sometimes, our Electron window gets into a funky state in which it fails to load the page but doesn't generate an error (at least not one that I seem to be receiving). So, if the loading time is below this threshold, we open a new Electron window to fix the problem.
 const SHOW_BROWSER = true;
+const SHOW_ERRORS = false;
 const INCLUDE_TIMESTAMP = true;
 
 //Dev settings
-// const TIMEOUT = 4000;
 // const FREQUENCY = 5000;
+// const TIMEOUT = FREQUENCY - 1000;
+// const LOADING_TIME_SANITY_CHECK_THRESHOLD = 1200;
 // const SHOW_BROWSER = true;
+// const SHOW_ERRORS = true;
 // const INCLUDE_TIMESTAMP = true;
 
 var url = process.argv[2];
@@ -30,32 +34,56 @@ Nightmare.action(
   }
 )
 
-//High level description of how nightmare works: Chaining commands onto nightmare adds to the queue of things to do.  Adding .then() executes those commands.  Note that you can start a new nightmare command queue within the then() anonymous function, itself terminated in a .then() call.
-var nightmare = new Nightmare({
-	//Dev
-	waitTimeout: TIMEOUT,
-	gotoTimeout: TIMEOUT,
-	loadTimeout: TIMEOUT,
-	executionTimeout: TIMEOUT,
-
-	pollInterval: 50,
-
-	//Electron options
-	//NOTE: The NightmareJS window (whether shown or hidden) is an instance of Electron's BrowserWindow.  So, you can use any options for BrowserWindow here, as well.  https://electron.atom.io/docs/all/#new-browserwindowoptions
-	webPreferences: {
-	},
-	show: SHOW_BROWSER,
-	openDevTools: {
-		mode: "detach"
-	},
-});
-
+var nightmare;
+var nightmareStart;
 var start;
 
 testUrl(url);
 setInterval(function () { testUrl(url); }, FREQUENCY)
 
+//Make sure we get a new Nightmare instance every hour. (In testing, Electron started malfunctioning after about 2.5 hours)
+function getNightmare() {
+  var now = new Date().getTime();
+  if(typeof nightmare != "undefined" && typeof nightmareStart != "undefined" && now - nightmareStart < 3600000) {
+    return nightmare;
+  }
+
+  closeNightmare();
+  nightmareStart = new Date().getTime();
+
+  //High level description of how nightmare works: Chaining commands onto nightmare adds to the queue of things to do.  Adding .then() executes those commands.  Note that you can start a new nightmare command queue within the then() anonymous function, itself terminated in a .then() call.
+  nightmare = new Nightmare({
+    //Dev
+    waitTimeout: TIMEOUT,
+    gotoTimeout: TIMEOUT,
+    loadTimeout: TIMEOUT,
+    executionTimeout: TIMEOUT,
+
+    pollInterval: 50,
+
+    //Electron options
+    //NOTE: The NightmareJS window (whether shown or hidden) is an instance of Electron's BrowserWindow.  So, you can use any options for BrowserWindow here, as well.  https://electron.atom.io/docs/all/#new-browserwindowoptions
+    webPreferences: {
+    },
+    show: SHOW_BROWSER,
+    openDevTools: {
+      mode: "detach"
+    },
+  });
+
+  return nightmare;
+}
+
+//End the existing Nightmare instance (window/headless window), if any. The next iteration will open a new Nightmare instance to perform its loading.
+function closeNightmare() {
+  if(typeof nightmare != "undefined") {
+    nightmare.end().then(); //Documentation says, ".then() must be called after .end() to run the .end() task"
+    nightmare = undefined;
+  }
+}
+
 function testUrl(url) {
+  nightmare = getNightmare();
 	start = new Date().getTime();
 	nightmare
 		.viewport(1680, 1050)
@@ -64,8 +92,13 @@ function testUrl(url) {
 		.clearCache()
 		// .end()
 		.then(function (r) {
-			var end = new Date().getTime();
-			console.log((INCLUDE_TIMESTAMP ? start + ": " : "") + (end - start));
+      var duration = new Date().getTime() - start;
+      if(duration < LOADING_TIME_SANITY_CHECK_THRESHOLD) {
+        closeNightmare();
+        throw { message: "The loading time sanity check threshold was not met. Nightmare (Electron) will be restarted." };
+      }
+
+      console.log((INCLUDE_TIMESTAMP ? start + "\t" : "") + duration);
 		})
 		.catch(handleError);
 }
@@ -73,7 +106,11 @@ function testUrl(url) {
 //Handle a NightmareJS error.
 function handleError(error) {
 	//Commented out the next line so we don't exit
-	// nightmare.end().then(); //Documentation says, ".then() must be called after .end() to run the .end() task"
+  // nightmare.end().then(); //Documentation says, ".then() must be called after .end() to run the .end() task"
+  
+  if(!SHOW_ERRORS) {
+    return;
+  }
 	
 	var message;
 	if(typeof error.details != "undefined" && error.details != "") {
@@ -88,5 +125,5 @@ function handleError(error) {
 		message = error.message;
 	}
 	// console.error(JSON.stringify({"status": "error", "message": message}));
-	console.log((INCLUDE_TIMESTAMP ? start + ": " : "") + "Error: " + message);
+	console.log((INCLUDE_TIMESTAMP ? start + "\t" : "") + "Error: " + message);
 }
